33ad9a5883decfeeb97f3bce2f6c93ed75c95fb3
Peter Dolkens <github@ddrit.com>
2016-04-05 15:35:56 +0100

    Restful WebAPI Support

---
+++ Dolkens.Framework.MVC/WebApiClient.cs
@@ -0,0 +1,150 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dolkens.Framework.MVC
+{
+    #region Interfaces
+
+    public interface IWebApiRequest<TResponse>
+    {
+        String ResourceUrl { get; }
+    }
+
+    #endregion
+
+    public class WebApiClient : IDisposable
+    {
+        #region Private Properties
+
+        protected HttpClient _client;
+
+        #endregion
+        
+        #region Constructor/s
+
+        public WebApiClient()
+        {
+            this._client = this._client ?? new HttpClient { };
+        }
+
+        public WebApiClient(String baseAddress)
+            : base()
+        {
+            if (!String.IsNullOrWhiteSpace(baseAddress))
+            {
+                this.BaseAddress = new Uri(baseAddress);
+            }
+        }
+
+        public WebApiClient(Uri baseAddress)
+            : base()
+        {
+            if (baseAddress != null)
+            {
+                this.BaseAddress = baseAddress;
+            }
+        }
+
+        #endregion
+
+        #region Public Properties
+
+        public Uri BaseAddress
+        {
+            get
+            {
+                this._client = this._client ?? new HttpClient { };
+                return this._client.BaseAddress;
+            }
+            set
+            {
+                this._client = this._client ?? new HttpClient { };
+                this._client.BaseAddress = value;
+            }
+        }
+
+        // public HttpRequestHeaders DefaultRequestHeaders { get; }
+        // public long MaxResponseContentBufferSize { get; set; }
+        // public TimeSpan Timeout { get; set; }
+
+        #endregion
+
+        #region CRUD Methods
+
+        public virtual TResponse Create<TResponse>(IWebApiRequest<TResponse> data = null) { return this.Create<TResponse>(data.ResourceUrl, data); }
+
+        public virtual TResponse Create<TResponse>(String resource, Object data = null)
+        {
+            HttpContent content = data == null ?
+                new ObjectContent(typeof(Object), data, new WwwFormUrlEncodedMediaTypeFormatter { }) :
+                new ObjectContent(data.GetType(), data, new WwwFormUrlEncodedMediaTypeFormatter { });
+
+            return this._client.PostAsync(resource, content).Result.Content.ReadAsAsync<TResponse>().Result;
+        }
+
+        public virtual TResponse Retrieve<TResponse>(IWebApiRequest<TResponse> data = null) { return this.Retrieve<TResponse>(data.ResourceUrl, data); }
+
+        public virtual TResponse Retrieve<TResponse>(String resource, Object data = null)
+        {
+            var query = WwwFormUrlEncodedMediaTypeFormatter.Serialize(data);
+
+            if (!String.IsNullOrWhiteSpace(query))
+            {
+                if (resource.Contains("?"))
+                {
+                    resource = String.Format("{0}&{1}", resource, query);
+                }
+                else
+                {
+                    resource = String.Format("{0}?{1}", resource, query);
+                }
+            }
+
+            return this._client.GetAsync(resource).Result.Content.ReadAsAsync<TResponse>().Result;
+        }
+
+        public virtual TResponse Update<TResponse>(IWebApiRequest<TResponse> data = null) { return this.Update<TResponse>(data.ResourceUrl, data); }
+
+        public virtual TResponse Update<TResponse>(String resource, Object data = null)
+        {
+            HttpContent content = data == null ?
+                new ObjectContent(typeof(Object), data, new WwwFormUrlEncodedMediaTypeFormatter { }) :
+                new ObjectContent(data.GetType(), data, new WwwFormUrlEncodedMediaTypeFormatter { });
+
+            return this._client.PutAsync(resource, content).Result.Content.ReadAsAsync<TResponse>().Result;
+        }
+
+        public virtual TResponse Delete<TResponse>(IWebApiRequest<TResponse> data = null) { return this.Delete<TResponse>(data.ResourceUrl, data); }
+
+        public virtual TResponse Delete<TResponse>(String resource, Object data = null)
+        {
+            var query = WwwFormUrlEncodedMediaTypeFormatter.Serialize(data);
+
+            if (!String.IsNullOrWhiteSpace(query))
+            {
+                if (resource.Contains("?"))
+                {
+                    resource = String.Format("{0}&{1}", resource, query);
+                }
+                else
+                {
+                    resource = String.Format("{0}?{1}", resource, query);
+                }
+            }
+
+            return this._client.DeleteAsync(resource).Result.Content.ReadAsAsync<TResponse>().Result;
+        }
+
+        #endregion
+
+        public void Dispose()
+        {
+            this._client = this._client ?? new HttpClient { };
+            this._client.Dispose();
+        }
+    }
+}
---
+++ Dolkens.Framework.MVC/WwwFormUrlEncodedMediaTypeFormatter.cs
@@ -0,0 +1,77 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Http.Formatting;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dolkens.Framework.MVC
+{
+    public class WwwFormUrlEncodedMediaTypeFormatter : FormUrlEncodedMediaTypeFormatter
+    {
+        private readonly static Encoding encoding = new UTF8Encoding(false);
+
+        public override Boolean CanWriteType(Type type)
+        {
+            return base.CanWriteType(type) || true; // type == typeof(JObject);
+        }
+
+        public override Task WriteToStreamAsync(Type type, Object value, Stream writeStream, System.Net.Http.HttpContent content, System.Net.TransportContext transportContext, System.Threading.CancellationToken cancellationToken)
+        {
+            if (value == null)
+            {
+                return Task.FromResult(0); // Task.CompletedTask
+            }
+            else if (base.CanWriteType(type))
+            {
+                return base.WriteToStreamAsync(type, value, writeStream, content, transportContext);
+            }
+            else
+            {
+                return Task.Factory.StartNew(() =>
+                {
+                    Byte[] bytes = encoding.GetBytes(WwwFormUrlEncodedMediaTypeFormatter.Serialize(value));
+                    writeStream.Write(bytes, 0, bytes.Length);
+                }, cancellationToken);
+            }
+        }
+
+        public static String Serialize(Object value)
+        {
+            if (value == null) return null;
+
+            JToken token = JToken.FromObject(value);
+            List<String> pairs = new List<String>();
+            WwwFormUrlEncodedMediaTypeFormatter.Flatten(pairs, token, new List<Object> { });
+            return String.Join("&", pairs);
+        }
+
+        private static void Flatten(List<String> pairs, JToken input, List<Object> indices)
+        {
+            if (input == null) return;
+
+            switch (input.Type)
+            {
+                case JTokenType.Array:
+                case JTokenType.Object:
+                case JTokenType.Property:
+                    foreach (var token in input)
+                    {
+                        indices.Add(token);
+                        WwwFormUrlEncodedMediaTypeFormatter.Flatten(pairs, token, indices);
+                        indices.RemoveAt(indices.Count - 1);
+                    }
+
+                    break;
+                default:
+                    var value = input.ToObject<String>();
+                    if (value == null) return;
+                    pairs.Add(String.Format("{0}={1}", Uri.EscapeDataString(input.Path), Uri.EscapeDataString(value)));
+                    break;
+            }
+        }
+    }
+}
