01ccbacac58b115d1f575e6ee5d8de7054137850
Peter Dolkens <github@ddrit.com>
2016-06-14 14:14:58 +0100

    DataTablesResult

---
+++ Dolkens.Framework.MVC/DataTablesResult.cs
@@ -0,0 +1,161 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Dolkens.Framework.MVC
+{
+
+    public class DataTablesResult<RowType> : ActionResult
+    {
+        const String JsonRequest_GetNotAllowed = "This request has been blocked because sensitive information could be disclosed to third party web sites when this is used in a GET request. To allow GET requests, set JsonRequestBehavior to AllowGet.";
+
+        public DataTablesResult()
+            : base()
+        {
+            this.JsonRequestBehavior = JsonRequestBehavior.DenyGet;
+        }
+
+        public Encoding ContentEncoding { get; set; }
+
+        public String ContentType { get; set; }
+
+        public IEnumerable<RowType> Data { get; set; }
+
+        public JsonRequestBehavior JsonRequestBehavior { get; set; }
+
+        private static Dictionary<Type, Dictionary<String, PropertyInfo>> _jsonMaps = new Dictionary<Type, Dictionary<String, PropertyInfo>> { };
+
+        public override void ExecuteResult(ControllerContext context)
+        {
+            if (context == null)
+                throw new ArgumentNullException("context");
+
+            if (this.JsonRequestBehavior == JsonRequestBehavior.DenyGet &&
+                String.Equals(context.HttpContext.Request.HttpMethod, "GET", StringComparison.OrdinalIgnoreCase))
+                throw new InvalidOperationException(DataTablesResult<RowType>.JsonRequest_GetNotAllowed);
+
+            HttpResponseBase response = context.HttpContext.Response;
+
+            if (!String.IsNullOrEmpty(this.ContentType))
+                response.ContentType = this.ContentType;
+            else
+                response.ContentType = "application/json";
+
+            if (this.ContentEncoding != null)
+                response.ContentEncoding = ContentEncoding;
+
+            Int32 displayStart = context.RequestContext.HttpContext.Request["iDisplayStart"].ToInt32(0);
+            Int32 displayLength = context.RequestContext.HttpContext.Request["iDisplayLength"].ToInt32(100);
+
+            if (this.Data == null) return;
+
+            RowType[] data;
+            var query = this.Data.AsEnumerable();
+
+            Int32 cCount = context.RequestContext.HttpContext.Request["iColumns"].ToInt32(0);
+            Dictionary<Int32, PropertyInfo> propertyMap = new Dictionary<Int32, PropertyInfo> { };
+
+            #region Search Support
+
+            if (cCount > 0)
+            {
+                if (!_jsonMaps.ContainsKey(typeof(RowType)))
+                {
+                    var properties = typeof(RowType).GetProperties(BindingFlags.Public | BindingFlags.Instance).Select(p => new
+                    {
+                        Property = p,
+                        JsonProperty = p.GetCustomAttribute<JsonPropertyAttribute>(true),
+                    });
+                    _jsonMaps[typeof(RowType)] = properties.ToDictionary(k => k.JsonProperty == null ? k.Property.Name : k.JsonProperty.PropertyName, v => v.Property);
+                }
+
+                var jsonMap = _jsonMaps[typeof(RowType)];
+
+                for (Int32 cIndex = 0; cIndex < cCount; cIndex++)
+                {
+                    var columnName = context.RequestContext.HttpContext.Request[String.Format("mDataProp_{0}", cIndex)];
+                    var columnSearch = context.RequestContext.HttpContext.Request[String.Format("sSearch_{0}", cIndex)];
+                    var columnRegex = context.RequestContext.HttpContext.Request[String.Format("bRegex_{0}", cIndex)].ToBoolean(false);
+
+                    if (!String.IsNullOrWhiteSpace(columnName))
+                    {
+                        if (jsonMap.ContainsKey(columnName))
+                        {
+                            propertyMap[cIndex] = jsonMap[columnName];
+
+                            if (!String.IsNullOrWhiteSpace(columnSearch))
+                            {
+                                if (!columnRegex)
+                                {
+                                    query = query.Where(r => jsonMap[columnName].GetValue(r).ToString().ToLowerInvariant().Contains(columnSearch.ToLowerInvariant()));
+                                }
+                                else
+                                {
+                                    query = query.Where(r => Regex.IsMatch(jsonMap[columnName].GetValue(r).ToString(), columnSearch, RegexOptions.IgnoreCase));
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+
+            #endregion
+
+            #region Sorting Support
+
+            Int32 sCount = context.RequestContext.HttpContext.Request["iSortingCols"].ToInt32(0);
+
+            if (sCount > 0)
+            {
+                var sQuery = query.OrderBy(r => true);
+
+                Int32 sortColumn;
+                Int32 sortDirection;
+
+                for (Int32 cIndex = 0; cIndex < sCount; cIndex++)
+                {
+                    if (Int32.TryParse(context.RequestContext.HttpContext.Request[String.Format("iSortCol_{0}", cIndex)], out sortColumn))
+                    {
+                        if (propertyMap.ContainsKey(sortColumn))
+                        {
+                            sortDirection = context.RequestContext.HttpContext.Request[String.Format("sSortDir_{0}", cIndex)].ToInt32(0);
+
+                            if (sortDirection == 0)
+                            {
+                                sQuery = sQuery.ThenBy(r => propertyMap[sortColumn].GetValue(r));
+                            }
+                            else
+                            {
+                                sQuery = sQuery.ThenByDescending(r => propertyMap[sortColumn].GetValue(r));
+                            }
+                        }
+                    }
+                }
+
+                data = sQuery.ToArray();
+            }
+            else
+            {
+                data = query.ToArray();
+            }
+
+            #endregion
+
+            response.Write(new
+            {
+                sEcho = context.RequestContext.HttpContext.Request["sEcho"],
+                recordsTotal = this.Data.Count(),
+                recordsFiltered = data.Count(),
+                data = data.Skip(displayStart).Take(displayLength),
+            }.ToJSON());
+        }
+    }
+}
--- Dolkens.Framework.MVC/JsonNetResult.cs
+++ Dolkens.Framework.MVC/JsonNetResult.cs
@@ -12,7 +12,8 @@ namespace Dolkens.Framework.MVC
     {
         const String JsonRequest_GetNotAllowed = "This request has been blocked because sensitive information could be disclosed to third party web sites when this is used in a GET request. To allow GET requests, set JsonRequestBehavior to AllowGet.";
 
-        public JsonNetResult() : base()
+        public JsonNetResult()
+            : base()
         {
             this.JsonRequestBehavior = JsonRequestBehavior.DenyGet;
         }
@@ -29,21 +30,22 @@ namespace Dolkens.Framework.MVC
         {
             if (context == null)
                 throw new ArgumentNullException("context");
-            
+
             if (this.JsonRequestBehavior == JsonRequestBehavior.DenyGet &&
                 String.Equals(context.HttpContext.Request.HttpMethod, "GET", StringComparison.OrdinalIgnoreCase))
                 throw new InvalidOperationException(JsonNetResult.JsonRequest_GetNotAllowed);
 
             HttpResponseBase response = context.HttpContext.Response;
 
-            if (!String.IsNullOrEmpty(ContentType))
-                response.ContentType = ContentType;
+            if (!String.IsNullOrEmpty(this.ContentType))
+                response.ContentType = this.ContentType;
             else
                 response.ContentType = "application/json";
-            
-            if (ContentEncoding != null)
+
+            if (this.ContentEncoding != null)
                 response.ContentEncoding = ContentEncoding;
-            if (Data != null)
+
+            if (this.Data != null)
                 response.Write(this.Data.ToJSON());
         }
     }
