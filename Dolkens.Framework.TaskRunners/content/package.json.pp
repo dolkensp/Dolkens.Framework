9ec044f0cd9eacc1ed19173c1b4a9e6df1f28d0c
Peter Dolkens <github@ddrit.com>
2017-12-08 15:22:22 +0000

    Update nuget references and TaskRunner supports Project + Solution scopes now

--- Dolkens.Framework.TaskRunners/content/package.json.pp
+++ Dolkens.Framework.TaskRunners/content/package.json.pp
@@ -1,14 +1,16 @@
+// If you don't use project-level installs, remove this file.
 {
   "name": "$rootnamespace$",
   "version": "$version$",
   "devDependencies": {
-    "grunt": "^0.4.5",
-    "grunt-cli": "~0.1.11",
+    // "gulp": "^3.9.1",
+    // "grunt": "^0.4.5",
+    // "grunt-cli": "~0.1.11",
     // "grunt-concurrent": "~0.4.0",
     // "grunt-contrib-compass": "~0.6.0",
     // "grunt-contrib-concat": "~0.3.0",
     // "grunt-contrib-jshint": "~0.6.4",
     // "grunt-contrib-uglify": "~0.2.4",
-    // "grunt-contrib-watch": "~0.5.3"
+    // "grunt-contrib-watch": "~0.5.3"    
   }
 }
--- Dolkens.Framework.TaskRunners/nuget.bat
+++ Dolkens.Framework.TaskRunners/nuget.bat
@@ -1,4 +1,4 @@
 @echo Uploading to nuget
 erase *.nupkg /Q /F
 for /f "delims=|" %%f in ('dir /b *.nuspec') do ..\.nuget\NuGet.exe pack %%f -build -properties Configuration=Release
-..\.nuget\Nuget.exe push *.nupkg
+..\.nuget\Nuget.exe push *.nupkg -Source https://api.nuget.org/v3/index.json
